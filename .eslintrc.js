0e2105bfc39335b9813ff2a25788561c07415f8b
Ronald Arias <ariasron@hotmail.com>
2022-04-02 12:59:48 -0500

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true
+  },
+  extends: [
+    'standard',
+    'plugin:react/recommended',
+    'plugin:testing-library/react',
+    'plugin:jest-dom/recommended'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: ['react', 'testing-library', 'jest-dom'],
+  rules: {
+    semi: ['error', 'never'],
+    'space-before-function-paren': ['error', 'never'],
+    'react/react-in-jsx-scope': 'off'
+  }
+}
