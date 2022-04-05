a73ca7f6781e26b7c20a9553d1119d5572a85ac8
Ronald Arias <ariasron@hotmail.com>
2022-04-05 16:18:13 -0500

    [Design System] defines font sizes

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -9,11 +9,10 @@ const GlobalStyle = createGlobalStyle`
 
   /* General variables */
   :root {
-    --ff-primary: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
-    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+    --ff-primary: 'Kumbh Sans', sans-serif;
 
     --fw-reg: 400;
-    --fw-bold: 700;
+    --fw-bold: 600;
 
     --clr-violet: #9e7f66;
     --clr-light-violet: #939BF4;
@@ -30,11 +29,37 @@ const GlobalStyle = createGlobalStyle`
     --fs-h4: 0.875rem;
     --fs-body: 1rem;
 
+    --lh-h1: 2.125rem;
+    --lh-h2: 1.8125rem;
+    --lh-h3: 1.5rem;
+    --lh-h4: 1.125rem;
+    --lh-body: 1.625rem;
+
     --bs: 0.25em 0.25em 0.75em rgba(0,0,0,.15);
 
     h1 {
       font-size: var(--fs-h1);
-      color: var(--clr-accent);
+      line-height: var(--lh-h1);
+    }
+
+    h2 {
+      font-size: var(--fs-h2);
+      line-height: var(--lh-h2);
+    }
+
+    h3 {
+      font-size: var(--fs-h3);
+      line-height: var(--lh-h3);
+    }
+
+    h4 {
+      font-size: var(--fs-h4);
+      line-height: var(--lh-h4);
+    }
+
+    body {
+      font-size: var(--fs-body);
+      line-height: var(--lh-body);
     }
   }
 
